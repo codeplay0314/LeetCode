bc903fc59628e57b516f59a53ec98b174fedc26a
codeplay0314 <codeplay0314@gmail.com>
2020-05-12 22:55:10 +0800

    0001

---
+++ README.md
@@ -0,0 +1,9 @@
+# LeetCode-Training
+personal training log for LeetCode
+
+## Problem List
+
+### 0001 Two Sum
+#### Difiiculty: 1
+#### Tags
+`hash` `map`
---
+++ codes/0001.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        map<int, int> mp;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            int t = mp[target - nums[i]];
+            if (t) {
+                res.push_back(t - 1);
+                res.push_back(i);
+                return res;
+            } else {
+                mp[nums[i]] = i + 1;
+            }
+        }
+        return res;
+    }
+};
